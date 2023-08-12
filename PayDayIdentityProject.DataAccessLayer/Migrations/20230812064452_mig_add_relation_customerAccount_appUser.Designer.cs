5a7e646018b795ef42e69a003cf0c25588ef5f78
ErincK <erinckocaoglu@gmail.com>
2023-08-12 10:08:50 +0300

    Dto Create...

--- PayDayIdentityProject.DataAccessLayer/Migrations/20230812064452_mig_add_relation_customerAccount_appUser.Designer.cs
+++ PayDayIdentityProject.DataAccessLayer/Migrations/20230812064452_mig_add_relation_customerAccount_appUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PayDayIdentityProject.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using PayDayIdentityProject.DataAccessLayer.Concrete;
 namespace PayDayIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230812064452_mig_add_relation_customerAccount_appUser")]
+    partial class mig_add_relation_customerAccount_appUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
