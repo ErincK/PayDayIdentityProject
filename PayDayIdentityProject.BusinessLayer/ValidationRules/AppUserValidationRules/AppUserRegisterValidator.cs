b331adfa56fc938e50ab9bb4055fbff865ac52f6
ErincK <erinckocaoglu@gmail.com>
2023-08-12 14:00:49 +0300

    Base Repo Done

--- PayDayIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
+++ PayDayIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -7,7 +7,16 @@ namespace PayDayIdentityProject.BusinessLayer.ValidationRules.AppUserValidationR
     {
         public AppUserRegisterValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage();
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı Adı alanı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("En fazla 30 karakter girişi yapabilirsiniz");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("En az 3 karakter girişi yapabilirsiniz");
+            RuleFor(x => x.Password).Equal(y=>y.ConfirmPassword).WithMessage("Parolalar eşleşmiyor");
+            RuleFor(x => x.Email).NotNull().EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz");
         }
     }
 }
