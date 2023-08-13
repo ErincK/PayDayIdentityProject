3753171aa96cb916b935661dfe94a28bbac5a512
ErincK <erinckocaoglu@gmail.com>
2023-08-13 13:28:41 +0300

    Errors TR

--- PayDayIdentityProject.PresentationLayer/Program.cs
+++ PayDayIdentityProject.PresentationLayer/Program.cs
@@ -1,12 +1,13 @@
 using PayDayIdentityProject.DataAccessLayer.Concrete;
 using PayDayIdentityProject.EntityLayer.Concrete;
+using PayDayIdentityProject.PresentationLayer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>();
-builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>();
 
 var app = builder.Build();
 
