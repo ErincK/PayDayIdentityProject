a6dead5385654545be02791a6663fba9316ba0b1
ErincK <erinckocaoglu@gmail.com>
2023-08-17 08:50:56 +0300

    Email Confirm done

--- PayDayIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ PayDayIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,22 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PayDayIdentityProject.EntityLayer.Concrete;
 using PayDayIdentityProject.PresentationLayer.Models;
 
 namespace PayDayIdentityProject.PresentationLayer.Controllers
 {
 	public class ConfirmMailController : Controller
 	{
-		[HttpGet]
-		public IActionResult Index(int id)
+		private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+		public IActionResult Index()
 		{
 			var value = TempData["Mail"];
-			ViewBag.v = value + " aa a";
+			ViewBag.v=value;
+			//confirmMailViewModel.Mail = value.ToString();
 			return View();
 		}
 
+
 		[HttpPost]
-		public IActionResult Index(ConfirmMailViewModel confirmMailViewModel)
+		public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
 		{
-			return View();
+            var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+			if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+			{
+				user.EmailConfirmed = true;  // We need to set the new state of Email.
+				await _userManager.UpdateAsync(user); // We need to Update the new state of "EmailConfirmed".
+                return RedirectToAction("Index","MyProfile");
+			}
+            return View();
 		}
 	}
 }
