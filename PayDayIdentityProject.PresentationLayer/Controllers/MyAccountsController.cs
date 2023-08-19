79c0f34d634bdfae80e604e671c16cf95dd0b797
ErincK <erinckocaoglu@gmail.com>
2023-08-19 12:25:23 +0300

    Template Revize

---
+++ PayDayIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PayDayIdentityProject.PresentationLayer.Controllers
+{
+    public class MyAccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
